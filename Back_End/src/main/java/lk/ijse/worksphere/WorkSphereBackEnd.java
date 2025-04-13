6aca3aa20712f93ccfe1a23be12519cff4c06c67
O.V.Chanuka Prabodha <ckprabodha@gmail.com>
2025-04-13 20:39:59 +0530

    enable scheduling support in the backend application

--- Back_End/src/main/java/lk/ijse/worksphere/WorkSphereBackEnd.java
+++ Back_End/src/main/java/lk/ijse/worksphere/WorkSphereBackEnd.java
@@ -2,8 +2,10 @@ package lk.ijse.worksphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WorkSphereBackEnd {
 
     public static void main(String[] args) {
